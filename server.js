924df1369597516e23cfa3c3f7eff1f5e768a544
Simba Pfaira <spfaira@yahoo.com>
2021-08-19 00:01:12 +0200

    Heroku configuration

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const cors = require('cors');
-app.use(cors());
+//const cors = require('cors');
+//app.use(cors());
 
 require('dotenv').config();
 const { Pool } = require('pg');
@@ -17,7 +17,7 @@ const pool = new Pool({
   port: process.env.DB_PORT,
 });
 
-const videos = require('./data.json');
+//const videos = require('./data.json');
 
 app.get('/', (req, res) => {
   res
@@ -93,8 +93,13 @@ app.delete('/videos/:videoId', async (req, res) => {
   }
 });
 
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
-
-//postgresql-defined-56626
